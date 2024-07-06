aa884d2c23440a605cef58aac1648eed00f67b8d
Luis Felipe <luis.lopes@dtidigital.com.br>
2024-07-06 11:04:24 -0300

    feat: lexer

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 import lexer.Lexer;
 import lexer.Tag;
 import lexer.Token;
-import lexer.Word;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        var lexer = new Lexer("C:\\Users\\dti Digital\\Documents\\Projetos\\Cefet\\Compiladores\\Trabalho-Compiladores\\src\\teste.txt");
+        var lexer = new Lexer("./src/teste.txt");
         Token token;
         while ((token = lexer.scan()) != null) {
             if(token.tag < 256) {
